1596072fbc560e7e65667495358b1207d18cf1bc
WellFitCommunity <Maria@thewellfitcommunity.org>
2025-04-22 06:50:41 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,49 @@
 // File: src/App.tsx
 import React from 'react';
+
 import Header from './components/Header';
 import WelcomePage from './components/WelcomePage';
+
+import WeatherWidget from './components/WeatherWidget';
+import CheckInTracker from './components/CheckInTracker';
+
 import DailyScripture from './components/DailyScripture';
 import TechTip from './components/TechTip';
 import DashMealOfTheDay from './components/DashMealOfTheDay';
+
 import WordFind from './components/WordFind';
+import Sudoku from './components/Sudoku';
+import Crossword from './components/Crossword';
+
 import EmergencyContact from './components/EmergencyContact';
+import AdminPanel from './components/AdminPanel';
+
 import Footer from './components/Footer';
 
-const App = () => {
-  return (
-    <div className="min-h-screen bg-white text-gray-800">
-      <Header />
-      <main className="p-4 space-y-6">
-        <WelcomePage />
-        <DailyScripture />
-        <TechTip />
-        <DashMealOfTheDay />
-        <WordFind />
-        <EmergencyContact />
-      </main>
-      <Footer />
-    </div>
-  );
-};
+const App = () => (
+  <div className="min-h-screen bg-white text-gray-800">
+    <Header />
+    <main className="p-4 space-y-6">
+      <WelcomePage />
+
+      <WeatherWidget />
+      <SmartWatchCheckIn />
+      <CheckInTracker />
+
+      <DailyScripture />
+      <TechTip />
+      <DashMealOfTheDay />
+
+      <WordFind />
+      <Sudoku />
+      <Crossword />
+
+      <EmergencyContact />
+      <AdminPanel />
+    </main>
+    <Footer />
+  </div>
+);
 
 export default App;
+
