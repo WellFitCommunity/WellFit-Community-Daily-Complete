295fd22b8c5e0e9b1423834a01450e06862836ed
WellFitCommunity <Maria@thewellfitcommunity.org>
2025-04-23 17:35:47 +0000

    Remove unsed isLocalhost to fix CI build error

--- src/serviceWorkerRegistration.ts
+++ src/serviceWorkerRegistration.ts
@@ -3,14 +3,6 @@
 // This file is based on the CRA PWA template.
 // It registers the service worker produced in your build.
 
-const isLocalhost = Boolean(
-    window.location.hostname === 'localhost' ||
-      window.location.hostname === '[::1]' ||
-      window.location.hostname.match(
-        /^127(?:\.(?:25[0-5]|2[0-4]\d|[01]?\d?\d)){3}$/
-      )
-  );
-  
   export function register() {
     if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
