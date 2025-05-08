16be6a178bfd5cd29b330dba7be700dae2701bbe
WellFitCommunity <Maria@thewellfitcommunity.org>
2025-05-08 07:19:39 +0000

    Remove header/footer temporarily to fix white screen for demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
+// src/App.tsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
 import WelcomePage from './components/WelcomePage';
 import SeniorEnrollmentPage from './components/SeniorEnrollmentPage';
 import Dashboard from './components/Dashboard';
@@ -13,20 +12,21 @@ import LogoutPage from './components/LogoutPage';
 import RequireAuth from './components/RequireAuth';
 import AdminPanel from './components/AdminPanel';
 import AdminProfileEditor from './components/AdminProfileEditor';
+// import Header from './components/Header';
+// import Footer from './components/Footer';
 
 const App: React.FC = () => (
   <Router>
-    <Header />
+    {/* TEMPORARILY DISABLED FOR DEMO STABILITY */}
+    {/* <Header /> */}
     <main className="flex-grow">
       <Routes>
-        {/* Unprotected */}
+        {/* Public routes */}
         <Route path="/" element={<WelcomePage />} />
         <Route path="/senior-enrollment" element={<SeniorEnrollmentPage />} />
-
-        {/* Public meal detail */}
         <Route path="/meal/:id" element={<MealDetailPage />} />
 
-        {/* Protected */}
+        {/* Protected routes */}
         <Route path="/dashboard" element={<RequireAuth><Dashboard /></RequireAuth>} />
         <Route path="/checkin" element={<RequireAuth><CheckInTracker /></RequireAuth>} />
         <Route path="/wordfind" element={<RequireAuth><WordFind /></RequireAuth>} />
@@ -42,7 +42,7 @@ const App: React.FC = () => (
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </main>
-    <Footer />
+    {/* <Footer /> */}
   </Router>
 );
 
