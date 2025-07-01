bcc7dcd2cbdfe8a181f6e245f8f86691cae8d7d2
WellFitCommunity <Maria@thewellfitcommunity.org>
2025-07-01 04:39:44 +0000

    test: commit

--- src/components/auth/RequireAuth.tsx
+++ src/components/auth/RequireAuth.tsx
@@ -28,7 +28,8 @@ const RequireAuth: React.FC<RequireAuthProps> = ({ children }) => {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
+    // than dropping them off on the home page. This is especially useful
+    // for authenticated routes that require login.
     return <Navigate to="/login" state={{ from: location }} replace />;
   }
 
